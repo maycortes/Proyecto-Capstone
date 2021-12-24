211c4b858cb6d12e56cc692198bb1540326ab3d6
JoseEduardoUAM <al2172002687@azc.uam.mx>
2021-12-23 18:30:29 -0600

    cambio_23_12_21

--- Public/Principal/Script/identificadores.js
+++ Public/Principal/Script/identificadores.js
@@ -6,3 +6,18 @@ export let ingresar = document.getElementById( 'ingresar' );
 
 // Mensaje de Espera
 export let mensanje = document.getElementById('mensanje');
+
+//Titulos para los inputs
+export let titulo_matricula = document.getElementById('titulo_matricula');
+export let titulo_pwd = document.getElementById('titulo_pwd');
+
+// Mensaje de Restricciones
+export let mensajeMatricula = document.getElementById('mensajeMatricula');
+export let mensajePassword = document.getElementById('mensajePassword');
+export let mensajeSeccion = document.getElementById('mensajeSeccion');
+
+// Expresiones Regulares para la validacion de formularios
+export let expresiones = {
+    matricula : /\d{10}/,
+    pwd : /\w|\W/
+}
